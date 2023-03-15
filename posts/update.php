9113bbe46015407ccbad578fd0a1f242c9dc082e
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-03-16 01:28:16 +0530

    Adding the update post functionality

--- posts/update.php
+++ posts/update.php
@@ -1,35 +1,45 @@
 <?php
-require_once "http://localhost/clean_post/includes/header.php";
+require_once "../includes/header.php";
+require_once "../config/config.php";
+
+if (isset($_GET['update_post_id'])) {
+    $post_update_id = $_GET['update_post_id'];
+    $query = "SELECT * FROM posts WHERE postID = '$post_update_id'";
+    $result = $conn->query($query);
+    $rows = array();
+    $row = $result->fetch_object();
+
+} else {
+    echo "Please select the post you want to edit or the post you want to edit is not found";
+}
 ?>
 
-<form method="POST" action="">
+<form method="POST" enctype="multipart/form-data" action="update_function.php?post_id=<?php echo $post_update_id ?>" >
     <!-- Email input -->
     <div class="form-outline mb-4">
-        <input type="text" name="email" id="form2Example1" class="form-control" placeholder="title" />
+        <input type="text" name="post_title" id="form2Example1" class="form-control"  value="<?php echo $row->title ?>" />
 
     </div>
 
     <div class="form-outline mb-4">
-        <input type="text" name="email" id="form2Example1" class="form-control" placeholder="subtitle" />
+        <input type="text" name="post_subtitle" id="form2Example1" class="form-control" placeholder="subtitle"  value="<?php echo $row->subtitle ?>"/>
     </div>
 
     <div class="form-outline mb-4">
-        <input type="text" name="email" id="form2Example1" class="form-control" placeholder="body" />
+        <textarea  class="form-control" id="exampleFormControlTextarea1" name="post_body" rows="10" placeholder="body"><?php echo $row->body ?></textarea>
     </div>
 
-
     <div class="form-outline mb-4">
-        <input type="file" name="email" id="form2Example1" class="form-control" placeholder="image" />
+        <input type="file" name="post_image_link" id="form2Example1" class="form-control" placeholder="image" />
     </div>
 
 
     <!-- Submit button -->
     <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">Update</button>
 
-
 </form>
 
 
 <?php
-require_once "http://localhost/clean_post/includes/footer.php";
+require_once "../includes/footer.php";
 ?>
---
+++ posts/update_function.php
@@ -0,0 +1,42 @@
+<?php
+require_once "../includes/header.php";  
+require_once "../config/config.php";
+
+if(isset($_GET['post_id'])) {
+    $post_id = $_GET['post_id'];
+}
+if (isset($_POST['submit'])) {
+
+
+    $post_title = $_POST['post_title'];
+    $post_subtitle = $_POST['post_subtitle'];
+    $post_body = $_POST['post_body'];
+    $post_image_link = $_FILES['post_image_link']['name'];
+    $dir = 'images/' . basename($post_image_link);
+
+    if($post_title == "" || $post_subtitle == "" || $post_body == "" || $post_image_link == "" ) {
+        echo "FIlL all the inputs";
+    } else {
+        $update = "UPDATE posts SET title = ?, subtitle = ?, body = ?, image = ? WHERE postID = '$post_id'";
+
+        $stm = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stm, $update)) {
+            exit();
+        }
+        mysqli_stmt_bind_param($stm, "ssss", $post_title, $post_subtitle, $post_body, $post_image_link);
+        move_uploaded_file($_FILES['post_image_link']['tmp_name'], $dir);
+        mysqli_stmt_execute($stm);
+        mysqli_stmt_close($stm);
+        header("Location: http://localhost/clean_post/posts/post.php?post_id=$post_id");
+        exit();
+    }
+
+   
+} else {
+    echo "Not submitted";
+}
+?>
+
+<?php
+require_once "../includes/footer.php";
+?>
